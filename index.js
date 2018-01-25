4adec4366f13b414382bef6c9883b43a6e534cb2
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-25 22:49:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function createRecipe() {
     description: description,
     ingredients: []
   };
-  console.log(ingredients);
+  console.log(ingredients[0].value);
 
   var displayRecipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
   document.getElementsByTagName("main")[0].innerHTML = displayRecipeTemplate(recipeContents);
